d354c8763d9f6282c6515f453a1015f0a1e0317b
Kannappan Sriramajayam <you@example.com>
2022-06-20 22:29:49 -0400

    Program to find Perfect Number

---
+++ PerfectNum.py
@@ -0,0 +1,19 @@
+#Check whether a number perfect or not
+
+n = int(input("Enter any number: "))
+
+sum = 0
+
+for i in range(1, n):
+    
+    if n % i == 0:
+        
+        sum += i
+
+if (sum == n):
+    
+    print(f"{n} is a perfect number")
+
+else:
+    
+    print(f"{n} is not a perfect number")
